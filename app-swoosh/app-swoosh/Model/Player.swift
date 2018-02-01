8af65e8da11f3fd456a9e8931aed16ba0a3cb121
Samantha Oosterling <samantha.oosterling@gmail.com>
2018-02-01 11:46:35 +0100

    button actions

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Samantha Oosterling on 2/1/18.
+//  Copyright © 2018 Samantha Oosterling. All rights reserved.
+//
+
+import Foundation
+
+//always use a struct when you use default swift types (strings etc) classes we use when doing custom things.
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
